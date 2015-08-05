318c478c6643fdaad83d945f4044a572634f53f8
akwong <akwong666@gmail.com>
2015-08-05 10:10:17 +0800

    firstpull

---
+++ NamePipeTest/NamePipeTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace NamePipeTest
+{
+    class Program
+    {
+        //sample code for test name pipe from:
+        //http://stackoverflow.com/questions/13806153/example-of-named-pipes
+
+        static void Main(string[] args)
+        {
+            StartServer();
+            Task.Delay(1000).Wait();
+
+
+            //Client
+            var client = new NamedPipeClientStream("PipesOfPiece");
+            client.Connect();
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input)) break;
+                writer.WriteLine(input);
+                writer.Flush();
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+
+        static void StartServer()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream("PipesOfPiece");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    writer.WriteLine(String.Join("", line.Reverse()));
+                    writer.Flush();
+                }
+            });
+        }
+
+
+    }
+}
